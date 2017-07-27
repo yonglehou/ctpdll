0b821e687a6fe38801ba5ac1873b5800c3d8bd98
charles Yang <yonglehou@163.com>
2017-07-27 22:49:22 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/ctpdll.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ctpdll.dir/ftdc2c_ctp.cpp.o"
+  "libctpdll.pdb"
+  "libctpdll.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ctpdll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
